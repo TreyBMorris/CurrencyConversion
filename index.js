ad17a90da75c11a552c7ea96feaeb69277e83d7d
TreyBMorris <trey@morris2morris.com>
2023-08-19 22:11:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,39 +3,20 @@ fetch('sample.json')
     .then(data => setData(data))
 
 let setData = (data) => {
-    console.info(data.result)
-    console.log(data.time_last_update_utc)
-    console.log(data.conversion_rates.USD)
     USD_To_Eur(data.conversion_rates.USD,data.conversion_rates.EUR)
     EUR_To_USD(data.conversion_rates.EUR)
-    set_EUR(data.conversion_rates.EUR);
-    
+    //(data.conversion_rates.EUR)
     document.getElementById("last_update").innerHTML = data.time_last_update_utc;
 }
-let set_EUR = (EUR) => {
-    this.EUR = EUR;
-}
-let get_EUR = () => {
-    
-}
 
-const EUR = get_EUR();
-console.log("EURO: "+ );
 let USD_To_Eur = (USD,EUR) => {
-    console.log("USD:" + USD);
     document.getElementById("usd_to_euro").innerHTML = USD + " USD is worth " + EUR +" Euros";
-    
-
 }
-let EUR_To_USD = (EUR) => {
-    console.log("EUR: "+ EUR);
+let EUR_To_USD = (EUR) => { 
     document.getElementById("euro_to_usd").innerHTML ="1 EUR is worth " + (1/EUR) +" USD";
-
 }
-
 let user_USD_To_Eur = (userNum,EUR) => {
     console.log("userNum:" + userNum);
     document.getElementById("user_usd_euro").innerHTML = userNum + " USD is worth " + (userNum*EUR) +" Euros";
     
-
 }
