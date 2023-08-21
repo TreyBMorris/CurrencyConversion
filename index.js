bc0c97421eaf6c951d4915afbcb2c04b64bc4ca4
Trey <trey@morris2morris.com>
2023-08-20 20:29:28 -0400

    Update

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ let xhReq = new XMLHttpRequest();
 xhReq.open("GET", jsonURL, false);
 xhReq.send(null);
 let jsonObject = JSON.parse(xhReq.responseText);
-let EUR = jsonObject.conversion_rates.EUR;
+let EUR = jsonObject.conversion_rates.EUR.toFixed(2);
 let USD = jsonObject.conversion_rates.USD;
+
 let time_last_update_utc = jsonObject.time_last_update_utc;
 console.log(jsonObject.result);
-console.log(EUR);
-console.log(USD);
+
 
 
 function onClick(){
@@ -20,4 +20,4 @@ function onClick(){
 
 document.getElementById('last_update').innerHTML = time_last_update_utc;
 document.getElementById('usd_to_euro').innerHTML = USD + " USD is worth " + EUR + " Euros.";
-document.getElementById('euro_to_usd').innerHTML = "1 EUR is worth " + (1/EUR) + " USD.";
+document.getElementById('euro_to_usd').innerHTML = "1 EUR is worth " + (1/EUR + " USD.");
--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import requests
 import json
 
 # Where USD is the base currency you want to use
-API_CALL = 'e2f6b62536a5148392dc167a/latest/USD'
-url = ('https://v6.exchangerate-api.com/v6/' + API_CALL)
-
+API_CALL = 'e2f6b62536a5148392dc167a'
+url = ('https://v6.exchangerate-api.com/v6/' + API_CALL + 'latest/USD')
 # Making our request
 response = requests.get(url)
 data = response.json()
