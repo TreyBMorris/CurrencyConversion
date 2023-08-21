a33d8c17de608da44315bd18f043338b35eec934
Trey <trey@morris2morris.com>
2023-08-20 20:57:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ xhReq.send(null);
 let jsonObject = JSON.parse(xhReq.responseText);
 let EUR = jsonObject.conversion_rates.EUR.toFixed(2);
 let USD = jsonObject.conversion_rates.USD;
+let AUD = jsonObject.conversion_rates.AUD.toFixed(2);
 
 let time_last_update_utc = jsonObject.time_last_update_utc;
 console.log(jsonObject.result);
@@ -13,7 +14,6 @@ console.log(jsonObject.result);
 
 function onClick(){
     var numberInput = document.getElementById('userNum').value;
-    console.log(numberInput);
     document.getElementById('user_usd_euro').innerHTML = numberInput + " USD is worth " + (numberInput*EUR) + " EUR.";
 }
 
@@ -21,3 +21,5 @@ function onClick(){
 document.getElementById('last_update').innerHTML = time_last_update_utc;
 document.getElementById('usd_to_euro').innerHTML = USD + " USD is worth " + EUR + " Euros.";
 document.getElementById('euro_to_usd').innerHTML = "1 EUR is worth " + (1/EUR + " USD.");
+
+console.log(AUD);
