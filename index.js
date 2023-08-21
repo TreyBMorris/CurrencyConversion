7e8f7bfb713a280b2cf0a08a8bbba4f9464a621f
TreyBMorris <trey@morris2morris.com>
2023-08-21 16:12:49 -0400

    Update
    -Updated index.html
    -Updated index.js

--- index.js
+++ index.js
@@ -8,11 +8,20 @@ let EUR = jsonObject.conversion_rates.EUR.toFixed(2);
 let USD = jsonObject.conversion_rates.USD;
 let AUD = jsonObject.conversion_rates.AUD.toFixed(2);
 let time_last_update_utc = jsonObject.time_last_update_utc;
+let responseText = document.querySelector('#currency_select');
+
 
 
 function onClick(){
     let numberInput = document.getElementById('userNum').value;
     document.getElementById('user_usd_euro').innerHTML = numberInput + " USD is worth " + (numberInput*EUR) + " EUR.";
+    convertAmount(numberInput,responseText.value);
+
+}
+
+function convertAmount(numberInput,value){
+    let convertTo = jsonObject.conversion_rates[value];
+    console.log(numberInput,value,convertTo);
 }
 
 function setTimeLastUpdate(){
@@ -26,6 +35,7 @@ function usdToEuro(){
 function eurToUsd(){
     document.getElementById('euro_to_usd').innerHTML = "1 EUR is worth " + (1/EUR + " USD.");
 }
+
 
 setTimeLastUpdate();
 usdToEuro();
