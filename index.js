dfed9debc7d96f9440454e1c56d8161b5539a12a
TreyBMorris <trey@morris2morris.com>
2023-08-21 17:38:39 -0400

    Updates
    -Update index.html
      -Added more Currency Options
    -Update index.js
    -Update main.json
    -Updata style.css

--- index.js
+++ index.js
@@ -4,41 +4,22 @@ xhReq.open("GET", jsonURL, false);
 xhReq.send(null);
 let jsonObject = JSON.parse(xhReq.responseText);
 
-let EUR = jsonObject.conversion_rates.EUR.toFixed(2);
-let USD = jsonObject.conversion_rates.USD;
-let AUD = jsonObject.conversion_rates.AUD.toFixed(2);
 let time_last_update_utc = jsonObject.time_last_update_utc;
 let responseText = document.querySelector('#currency_select');
-
 
 
 function onClick(){
     let numberInput = document.getElementById('userNum').value;
     convertAmount(numberInput,responseText.value);
-
 }
 
 function convertAmount(numberInput,value){
     let convertTo = jsonObject.conversion_rates[value];
     document.getElementById('user_response').innerHTML = numberInput + " USD is worth " + (numberInput*convertTo) + " " + value;
-    console.log(numberInput,value,convertTo);
 }
 
 function setTimeLastUpdate(){
     document.getElementById('last_update').innerHTML = time_last_update_utc;
 }
 
-function usdToEuro(){
-    document.getElementById('usd_to_euro').innerHTML = USD + " USD is worth " + EUR + " Euros.";
-}
-
-function eurToUsd(){
-    document.getElementById('euro_to_usd').innerHTML = "1 EUR is worth " + (1/EUR + " USD.");
-}
-
-
 setTimeLastUpdate();
-usdToEuro();
-eurToUsd();
-
-console.log(AUD);
