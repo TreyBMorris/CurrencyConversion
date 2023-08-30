a156c5480957e037f9ffae521803de4b073ec73a
TreyBMorris <trey@morris2morris.com>
2023-08-29 21:00:49 -0400

    Update

--- index.js
+++ index.js
@@ -6,16 +6,34 @@ let jsonObject = JSON.parse(xhReq.responseText);
 
 let time_last_update_utc = jsonObject.time_last_update_utc;
 let responseText = document.querySelector('#currency_select');
+let responseFrom = document.querySelector('#currency_from');
 
 function onClick(){
     let numberInput = document.getElementById('userNum').value;
-    convertAmount(numberInput,responseText.value);
+    if (responseFrom.value != "USD")
+    {
+        convertAmount(numberInput, responseFrom.value,responseText.value)
+    } 
+    else 
+    {
+        convertAmounttoUSD(numberInput,responseText.value);
+    }
 }
 
-function convertAmount(numberInput,value){
+function convertAmounttoUSD(numberInput,value){
     let convertTo = jsonObject.conversion_rates[value];
     document.getElementById('user_response').innerHTML = numberInput + " USD is worth " + (numberInput*convertTo) + " " + value;
 }
+
+function convertAmount(numberInput,valueFrom,valueTo) {
+    let convertTo = jsonObject.conversion_rates[valueFrom];
+    let convertToToo = jsonObject.conversion_rates[valueFrom];
+        document.getElementById('user_response').innerHTML = numberInput + " "+ valueFrom + " is worth " + (numberInput/convertTo) + " " + valueTo;
+
+    console.log(convertTo);
+}
+
+
 
 function setTimeLastUpdate(){
     document.getElementById('last_update').innerHTML = time_last_update_utc;
